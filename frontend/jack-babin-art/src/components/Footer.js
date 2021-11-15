2e9b1e326df5ff7d14fbf20f59aac533d6fdf192
chrispando <45864608+chrispando@users.noreply.github.com>
2021-11-15 10:21:29 -0600

    Social links

--- frontend/jack-babin-art/src/components/Footer.js
+++ frontend/jack-babin-art/src/components/Footer.js
@@ -7,18 +7,23 @@ const Footer = () => {
       <Container>
         <Row className="d-flex justify-content">
           <Col className="py-3 m-3">
-            <a href="http://www.facebook.com" className="m-3 facebook-link">
+            <a
+              href="https://www.instagram.com/jackbabin.art/"
+              className="m-3 instagram-link"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/Jackbabin.art/"
+              className="m-3 facebook-link"
+            >
               {" "}
               <i className="fab fa-facebook-f"></i>{" "}
             </a>
-            <a href="http://www.twitter.com" className="m-3 twitter-link">
-              {" "}
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="http://www.instagram.com" className="m-3 instagram-link">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="http://www.tiktok.com" className="m-3 tiktok-link">
+            <a
+              href="https://www.tiktok.com/@jackbabin.art?"
+              className="m-3 tiktok-link"
+            >
               <i className="fab fa-tiktok"></i>
             </a>
           </Col>
