6fbe6a4dc32873576408b69367239267d3759171
Hussam Khan <hussamkhan98@gmail.com>
2021-11-18 19:45:10 +0500

    ready for production

--- frontend/jack-babin-art/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./bootstrap.min.css";
-import "./index.css";
-import LandingPage from "./components/LandingPage";
-
-ReactDOM.render(<LandingPage />, document.getElementById("root"));
