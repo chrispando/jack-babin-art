41f509440eb66f7dd1361db0606ec6f1926a400f
chrispando <45864608+chrispando@users.noreply.github.com>
2021-11-18 18:04:10 -0600

    Updated About section

--- frontend/jack-babin-art/src/components/About.js
+++ frontend/jack-babin-art/src/components/About.js
@@ -19,25 +19,11 @@ const About = () => {
 
       <Row>
         <p>
-          Plaid normcore hoodie vape tacos. Portland marfa umami, twee pour-over
-          everyday carry pitchfork tattooed af trust fund seitan post-ironic
-          slow-carb. Iceland 3 wolf moon literally gluten-free affogato,
-          succulents kitsch sriracha kickstarter normcore bushwick distillery
-          poke lo-fi seitan. Williamsburg locavore stumptown shoreditch.
-          Taxidermy pitchfork pinterest, umami fam mlkshk flexitarian four
-          dollar toast forage pok pok heirloom occupy lyft chartreuse.
+        My name is Jack Babin and I am an artist in Houston, TX. I am inspired by surrealism, 
+        absurdity and abstract. I love to create art that is a reflection of my own subconscious 
+        by using color and chaotic orientation as a template. 
         </p>
-        <p>
-          Celiac pop-up coloring book la croix authentic YOLO etsy. Portland
-          gastropub art party whatever salvia cold-pressed iPhone four dollar
-          toast williamsburg air plant umami. Live-edge heirloom subway tile
-          fanny pack flexitarian pickled tofu. Kogi man bun art party woke cloud
-          bread next level wayfarers quinoa fam sriracha kale chips. Gentrify
-          cardigan occupy, freegan adaptogen pour-over 90's enamel pin put a
-          bird on it tote bag vinyl. Before they sold out tacos 90's shabby chic
-          viral leggings godard swag semiotics artisan direct trade. Try-hard
-          chia raclette pinterest vape pabst glossier leggings asymmetrical.
-        </p>
+        
       </Row>
     </div>
   );
