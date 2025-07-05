752cb89cda342cbd25316b35242543cf67e6bbe0
chrispando <45864608+chrispando@users.noreply.github.com>
2025-07-05 06:58:29 -0500

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,20 +2,10 @@ import bcrypt from "bcryptjs";
 
 const users = [
   {
-    name: "Jack",
+    name: "User",
     email: "admin@example.com",
-    password: bcrypt.hashSync("123456", 10),
+    password: bcrypt.hashSync("12345", 10),
     isAdmin: true,
-  },
-  {
-    name: "Chris",
-    email: "chris@example.com",
-    password: bcrypt.hashSync("123456", 10),
-  },
-  {
-    name: "Alice",
-    email: "alice@example.com",
-    password: bcrypt.hashSync("123456", 10),
   },
 ];
 
